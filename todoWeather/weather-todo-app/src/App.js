b6dabbc92c9797344aa5358b4ad5fd19731ff5e7
mbravofuentes <mbravofuentes@mavs.coloradomesa.edu>
2021-01-21 09:03:06 -0700

    Broke apart chunks of code into components

--- todoWeather/weather-todo-app/src/App.js
+++ todoWeather/weather-todo-app/src/App.js
@@ -1,52 +1,31 @@
 import Todo from './components/Todo';
+import FilterButton from './components/FilterButton';
+import Form from './components/Form';
 
 function App(props) {
-  const taskList = props.task.map((task) => <Todo id={task.id} name={task.name} completed={task.completed}/>);
-
-    return (
+  const taskList = props.task.map((task) => (
+  <Todo 
+    id={task.id} 
+    name={task.name} 
+    completed={task.completed}
+    key={task.id}
+    />
+    )
+  );
+  return (
       <div className="todoapp stack-large">
         <h1>TodoMatic</h1>
-        <form>
-          <h2 className="label-wrapper">
-            <label htmlFor="new-todo-input" className="label__lg">
-              What needs to be done?
-            </label>
-          </h2>
-          <input
-            type="text"
-            id="new-todo-input"
-            className="input input__lg"
-            name="text"
-            autoComplete="off"
-          />
-          <button type="submit" className="btn btn__primary btn__lg">
-            Add
-          </button>
-        </form>
+        <Form/>
         <div className="filters btn-group stack-exception">
-          <button type="button" className="btn toggle-btn" aria-pressed="true">
-            <span className="visually-hidden">Show </span>
-            <span>all</span>
-            <span className="visually-hidden"> tasks</span>
-          </button>
-          <button type="button" className="btn toggle-btn" aria-pressed="false">
-            <span className="visually-hidden">Show </span>
-            <span>Active</span>
-            <span className="visually-hidden"> tasks</span>
-          </button>
-          <button type="button" className="btn toggle-btn" aria-pressed="false">
-            <span className="visually-hidden">Show </span>
-            <span>Completed</span>
-            <span className="visually-hidden"> tasks</span>
-          </button>
+          <FilterButton/>
+          <FilterButton/>
+          <FilterButton/>
         </div>
         <h2 id="list-heading">
           3 tasks remaining
         </h2>
         <ul
-          role="list"
           className="todo-list stack-large stack-exception"
-          aria-labelledby="list-heading"
         >
           {taskList}
         </ul>
