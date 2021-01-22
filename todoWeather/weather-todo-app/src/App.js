1eb128fd9de5081d113d056b28a8053584554c2d
mbravofuentes <mbravofuentes@mavs.coloradomesa.edu>
2021-01-21 22:55:15 -0700

    Filter Button addon

--- todoWeather/weather-todo-app/src/App.js
+++ todoWeather/weather-todo-app/src/App.js
@@ -6,34 +6,37 @@ import React, { useState } from 'react';
 
 function App(props) {
 
+  
   const [tasks, setTasks] = useState(props.task);
-
+  
   const taskList = tasks.map((task) => (
-  <Todo 
+    <Todo 
     id={task.id} 
     name={task.name} 
     completed={task.completed}
     key={task.id}
     />
     )
-  );
-
-  function addTask(name){
-    const newTask = { id: "id", name:name, completed:false };
-    setTasks([...tasks, newTask]);
-  }
-
+    );
+    
+    function addTask(name){
+      const newTask = { id: "todo-" + nanoid(), name:name, completed:false };
+      setTasks([...tasks, newTask]);
+    }
+  const taskNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${taskNoun} remaining`;
+    
   return (
       <div className="todoapp stack-large">
         <h1>TodoMatic</h1>
         <Form addTask={addTask}/>
         <div className="filters btn-group stack-exception">
-          <FilterButton/>
-          <FilterButton/>
-          <FilterButton/>
+          <FilterButton name="All"/>
+          <FilterButton name="Active"/>
+          <FilterButton name="Completed"/>
         </div>
         <h2 id="list-heading">
-          3 tasks remaining
+          {headingText}
         </h2>
         <ul
           className="todo-list stack-large stack-exception"
