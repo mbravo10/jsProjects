cf99deedaa3376a963335aaf9c157aa67bf858d0
mbravofuentes <mbravofuentes@mavs.coloradomesa.edu>
2021-01-27 19:33:02 -0700

    Adding Images and view to component

--- weather-app/src/components/Home/Weather.js
+++ weather-app/src/components/Home/Weather.js
@@ -1,8 +1,32 @@
 import React, { useState } from 'react';
+import { Card, CardGroup, Container, Col } from 'react-bootstrap';
+import weatherIcon from './images/weather.jpg';
 
 function Weather(){
     return (
-        <h1>Home Weather</h1>
+        <Container>
+            <CardGroup>
+            <Card >
+            <Col xs={6} md={4}>
+            <Card.Img variant="top" src={weatherIcon}/>
+            </Col>
+            <Card.Body>
+                <Card.Title>Current Weather</Card.Title>
+                <Card.Text>Weather from the 5 min</Card.Text>
+            </Card.Body>
+            </Card>
+
+            <Card>
+            <Col xs={6} md={4}>
+            <Card.Img variant="top" src={weatherIcon}/>
+            </Col>
+            <Card.Body>
+                <Card.Title>Other Info</Card.Title>
+                <Card.Text>Weather from the 5 min</Card.Text>
+            </Card.Body>
+            </Card>
+            </CardGroup>
+        </Container>
     )
 }
 export default Weather;
