2ef1a9969f239a7103f21aaec3b6902b926df93b
mbravofuentes <mbravofuentes@mavs.coloradomesa.edu>
2021-02-16 00:30:41 -0700

    Wildcard add

--- weather-app/src/components/Home/Weather.js
+++ weather-app/src/components/Home/Weather.js
@@ -1,53 +1,41 @@
 import React, { useState, useEffect } from 'react';
-import { Card, CardGroup, Container, Col, Button } from 'react-bootstrap';
+import { Card, CardGroup, Container, Col, Button, InputGroup, FormControl } from 'react-bootstrap';
 import weatherIcon from './images/weather.jpg';
 import axios from 'axios';
 
 function Weather(){
-    const [weather, setWeather ] = useState('');
-    const [city, setCity ] = useState('');
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
+    const [city, setCity] = useState('No city Selected');
+   
+    function onClickChange(e){
+        alert(e.target.value);
+        e.preventDefault();
+    }
 
-    const getWeather = useEffect(() => {
-        fetch('http://api.openweathermap.org/data/2.5/weather?q=Denver&appid=052e1da49244f19c526d173950e25c8f&units=imperial')
-        .then(res => res.json())
-        .then(
-            (res2) =>{
-                setIsLoaded(true);
-                setWeather(res2.main.temp);
-                setCity(res2.name);
-            },
-            (err) => {
-                setIsLoaded(true);
-                setError(err);
-            }
-            )
-    })
 
     return (
         <Container>
             <CardGroup>
-            <Card >
-            <Col xs={6} md={4}>
-            <Card.Img variant="top" src={weatherIcon}/>
-            </Col>
-            <Card.Body>
-                <Card.Title>Current Weather</Card.Title>
-                <Card.Text>Weather from the 5 min</Card.Text>
-                <Card.Text>{weather}</Card.Text>
-                <Card.Text>{city}</Card.Text>
-                <Button variant="primary" onClick={getWeather}>Get Current Weather</Button>
-            </Card.Body>
-            </Card>
-
             <Card>
             <Col xs={6} md={4}>
             <Card.Img variant="top" src={weatherIcon}/>
             </Col>
             <Card.Body>
-                <Card.Title>Other Info</Card.Title>
+                <Card.Title>{city}</Card.Title>
                 <Card.Text>Weather from the 5 min</Card.Text>
+                <InputGroup className="mb-3">
+                <InputGroup.Prepend>
+                <InputGroup.Text id="basic-addon1">City</InputGroup.Text>
+                </InputGroup.Prepend>
+                <FormControl
+                placeholder="Get Weather"
+                aria-label="Weather"
+                aria-describedby="basic-addon1"
+                />
+                </InputGroup>
+                <Button variant="primary"
+                type="submit" 
+                for="basic-addon1" 
+                onClick={onClickChange}>Submit</Button>
             </Card.Body>
             </Card>
             </CardGroup>
