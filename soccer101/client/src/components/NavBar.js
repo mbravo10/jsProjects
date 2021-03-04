2c25546ba72fc8c3feb2565350449253f4505cba
mbravofuentes <mbravofuentes@mavs.coloradomesa.edu>
2021-03-04 16:14:30 -0700

    Boilerplate components

---
+++ soccer101/client/src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+
+export default function NavBar(){
+    return (
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="/home">Home</Navbar.Brand>
+            <Nav className="mr-auto">
+					<Nav.Link href="/teams">Teams</Nav.Link>
+					<Nav.Link href="/country">Standings</Nav.Link>
+					<Nav.Link href="/discussion">Forum</Nav.Link>
+				</Nav>
+        </Navbar>
+    );
+}
