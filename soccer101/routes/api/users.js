9b1cfff10d1e9bacc09dd75d1ef5a153d8db44ac
mbravofuentes <mbravofuentes@mavs.coloradomesa.edu>
2021-03-07 23:22:34 -0700

    Added express valid to check for correct input in fields rather than creatin our own logic

--- soccer101/routes/api/users.js
+++ soccer101/routes/api/users.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check')
 
 //To create a route we do 
 // @Route    POST api/users
 // @Desc     Register user route
 // @access   Public
-router.post('/', (req, res) => {
-    console.log(req.body);
+router.post(
+    '/', 
+    [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+    ],
+    (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        //If information not included correctly, bad req
+        return res.status(400).json({ errors: errors.array() });
+    }
     res.send('User route')
 
 });
