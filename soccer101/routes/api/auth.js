d9c56eed912415aa295dc66ee964a94e6ede3825
mbravofuentes <mbravofuentes@mavs.coloradomesa.edu>
2021-03-08 15:49:13 -0700

    Added custom middleawre folder

--- soccer101/routes/api/auth.js
+++ soccer101/routes/api/auth.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth.js');
 
 //To create a route we do 
 // @Route    GET api/auth
 // @Desc     Test route
 // @access   Public
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth ,(req, res) => res.send('Auth route'));
 
 module.exports = router;
