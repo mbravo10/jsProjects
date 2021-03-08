1413fefe12dd44fbc0299af2dd67bbef675c3fe5
mbravofuentes <mbravofuentes@mavs.coloradomesa.edu>
2021-03-07 22:56:06 -0700

    Added post request body parser as middleware function with app.use express.json

--- soccer101/routes/api/users.js
+++ soccer101/routes/api/users.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 //To create a route we do 
-// @Route    GET api/users
-// @Desc     Test route
+// @Route    POST api/users
+// @Desc     Register user route
 // @access   Public
-router.get('/', (req, res) => res.send('User route'));
+router.post('/', (req, res) => {
+    console.log(req.body);
+    res.send('User route')
+
+});
 
 module.exports = router;
