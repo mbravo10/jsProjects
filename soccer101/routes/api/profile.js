68a8be9e2956bdc6ce9b69fd99fd1d8733e1e44b
mbravofuentes <mbravofuentes@mavs.coloradomesa.edu>
2021-03-11 10:31:39 -0700

    Added profile route and profile user creation

--- soccer101/routes/api/profile.js
+++ soccer101/routes/api/profile.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth'); //For protected routes
+
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 //To create a route we do 
-// @Route    GET api/profiles
-// @Desc     Test route
-// @access   Public
-router.get('/', (req, res) => res.send('Profile route'));
+// @Route    GET api/profile/me
+// @Desc     Get current users profile 
+// @access   Private, getting profile by user id with token
+router.get('/me', auth, async (req, res) => {
+    
+    try{
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user', 'name');
+
+        if(!profile){
+            return res.status(400).json({ msg: 'There is no profile for this user' });
+        }
+
+        res.json(profile);
+
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send('Server err');
+    }
+    
+});
 
 module.exports = router;
