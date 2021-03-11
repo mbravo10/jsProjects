45bcac47c7ee8f73aeb3d7aec9bc1dfb5287434f
mbravofuentes <mbravofuentes@mavs.coloradomesa.edu>
2021-03-11 09:31:12 -0700

    Added auth middleware for authentication

--- soccer101/routes/api/auth.js
+++ soccer101/routes/api/auth.js
@@ -2,10 +2,22 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth.js');
 
+const User = require('../../models/User');
+
 //To create a route we do 
 // @Route    GET api/auth
 // @Desc     Test route
 // @access   Public
-router.get('/', auth ,(req, res) => res.send('Auth route'));
+
+//This is a protected route unless we get a token 
+router.get('/', auth , async (req, res) => {
+    try{
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user); 
+    }catch(err){
+        console.err(err.message);
+        res.status(500).send('Server error');
+    }
+});
 
 module.exports = router;
