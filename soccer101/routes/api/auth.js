67b017191711691da9e664d5d3bac8baaa7e6fd8
mbravofuentes <mbravofuentes@mavs.coloradomesa.edu>
2021-03-18 10:29:52 -0600

    Added posts post method to create a post linked to a user

--- soccer101/routes/api/auth.js
+++ soccer101/routes/api/auth.js
@@ -4,7 +4,7 @@ const auth = require("../../middleware/auth.js");
 const config = require("config");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const { check, validationResult } = require("express-validator/check");
+const { check, validationResult } = require("express-validator");
 
 const User = require("../../models/User");
 
--- soccer101/routes/api/posts.js
+++ soccer101/routes/api/posts.js
@@ -1,10 +1,41 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
+
+const Post = require("../../models/Posts");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
 //To create a route we do
-// @Route    GET api/posts
-// @Desc     Test route
-// @access   Public
-router.get("/", (req, res) => res.send("Posts route"));
+// @Route    Post api/posts
+// @Desc     Create a post
+// @access   Private
+router.post(
+  "/",
+  [auth, [check("text", "text is required").not().isEmpty()]],
+  async (req, res) => {
+    errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        user: req.user.id,
+      });
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 module.exports = router;
