b07b68f9c126ca092e26bedf2e8cb36a8dca2028
mbravofuentes <mbravofuentes@mavs.coloradomesa.edu>
2021-03-23 10:31:48 -0600

    Cleaned things up and adding client connection to api

--- soccer101/client/src/components/Home.js
+++ soccer101/client/src/components/Home.js
@@ -1,16 +1,14 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from "react";
 
-export default function Home(){
-    const [apiResponse, setApiResponse] = useState('');
+export default function Home() {
+  const [apiResponse, setApiResponse] = useState("Hey");
 
-    useEffect(() => {
-    fetch("http://localhost:9000/testAPI")
-        .then(res => res.text())
-        .then(res => setApiResponse(res))
-        .catch(err => err)
-    });
+  useEffect(() => {
+    fetch("http://localhost:5000/api/profile")
+      .then((res) => res.text())
+      .then((res) => setApiResponse(res))
+      .catch((err) => err);
+  });
 
-    return (
-    <h1>{apiResponse}</h1>
-    );
-};
+  return <h1>{apiResponse}</h1>;
+}
--- soccer101/server.js
+++ soccer101/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const cors = require("cors");
 const app = express();
 
 //Connect Databse
@@ -7,6 +8,7 @@ connectDB();
 
 //Init middleware
 app.use(express.json({ extended: false }));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("API Running");
