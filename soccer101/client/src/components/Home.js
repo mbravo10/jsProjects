6f8adef7d81abac09a359b2577517f5dacde3eb0
mbravofuentes <mbravofuentes@mavs.coloradomesa.edu>
2021-04-05 22:01:29 -0600

    Getting user info from api

--- soccer101/client/src/components/Home.js
+++ soccer101/client/src/components/Home.js
@@ -1,46 +1,34 @@
 import { useEffect, useState } from "react";
-import { Table } from "react-bootstrap";
+import { ListGroup } from "react-bootstrap";
+const axios = require("axios");
 
 export default function Home() {
-  const [teams, setTeams] = useState([]);
-  const [userName, setUserName] = useState([]);
-  const [userBio, setUserBio] = useState([]);
+  const [userInfo, setUserInfo] = useState([
+    {
+      bio: "",
+      teams: [],
+    },
+  ]);
 
   useEffect(() => {
-    fetch("http://localhost:5000/api/profile")
-      .then((res) => res.json())
-      .then((res) => {
-        res.forEach((item) => {
-          setTeams((info) => info.concat(item.teams));
-        });
-      })
-      .catch((err) => err);
+    async function renderUsers() {
+      try {
+        const res = await axios.get("http://localhost:5000/api/profile/");
+        const { data } = res;
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    renderUsers();
   }, []);
 
   return (
     <>
-      <h1> The current user singed up favorite teams</h1>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Name</th>
-            <th>Teams</th>
-            <th>Bio</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          {teams.map((x) => (
-            <tr> {x} </tr>
-          ))}
-        </tbody>
-      </Table>
+      <h1> The current users singed up favorite teams</h1>
+      <ul>
+        <li>hi</li>
+      </ul>
     </>
   );
 }
