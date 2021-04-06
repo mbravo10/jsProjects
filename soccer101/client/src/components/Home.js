fc10ef1ac8748066ae22e21618b75e44ec85c0d7
mbravofuentes <mbravofuentes@mavs.coloradomesa.edu>
2021-04-06 09:49:25 -0600

    Fetching data into array

--- soccer101/client/src/components/Home.js
+++ soccer101/client/src/components/Home.js
@@ -3,32 +3,41 @@ import { ListGroup } from "react-bootstrap";
 const axios = require("axios");
 
 export default function Home() {
-  const [userInfo, setUserInfo] = useState([
-    {
-      bio: "",
-      teams: [],
-    },
-  ]);
+  const [userInfo, setUserInfo] = useState([]);
+  const [loadedTeams, setLoadedTeams] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
 
+  const userDataLoaded = userInfo.map((x, idx) => (
+    <ListGroup.Item as="li" key={idx}>
+      {x.bio}
+    </ListGroup.Item>
+  ));
+
+  const userTeamLoaded = loadedTeams.map((x, idx) => (
+    <ListGroup.Item as="li" key={idx}>
+      {x}
+    </ListGroup.Item>
+  ));
   useEffect(() => {
-    async function renderUsers() {
-      try {
-        const res = await axios.get("http://localhost:5000/api/profile/");
-        const { data } = res;
-        console.log(data);
-      } catch (error) {
-        console.error(error);
-      }
-    }
     renderUsers();
   }, []);
+
+  const renderUsers = async () => {
+    const res = await axios.get("http://localhost:5000/api/profile/");
+    let resMap = res.data;
+    let teams = res.data.map((x) => x.teams);
+    setLoadedTeams([...loadedTeams, ...teams]);
+    setUserInfo([...userInfo, ...resMap]);
+    setIsLoaded(true);
+  };
 
   return (
     <>
       <h1> The current users singed up favorite teams</h1>
-      <ul>
-        <li>hi</li>
-      </ul>
+      <ListGroup horizontal="sm">
+        {isLoaded ? userDataLoaded : "Loading..."}
+      </ListGroup>
+      <ListGroup horizontal="sm">{isLoaded ? userTeamLoaded : "..."}</ListGroup>
     </>
   );
 }
