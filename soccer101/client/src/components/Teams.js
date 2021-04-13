b9d196597e722285c32230c29143a57fb9712b26
mbravofuentes <mbravofuentes@mavs.coloradomesa.edu>
2021-04-13 15:49:30 -0600

    Made and added teams collection and picture to site

--- soccer101/client/src/components/Teams.js
+++ soccer101/client/src/components/Teams.js
@@ -1,25 +1,40 @@
 import { useEffect, useState } from "react";
-import { ListGroup } from "react-bootstrap";
+import { ListGroup, Image, Row, Col } from "react-bootstrap";
 import { connect } from "react-redux";
 import axios from "axios";
 
 function Teams({ authenticated }) {
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]);
   useEffect(() => {
     const getD = async () => {
       const res = await axios.get("/api/teams");
-      setData({ ...res.data });
+      setData([...res.data.teams]);
     };
     getD();
   }, []);
 
-  const showTeams = data.map((x, idx) => (
-    <ListGroup>
-      <ListGroup.Item>{x.teams}</ListGroup.Item>
-    </ListGroup>
+  const goThrough = data.map((x, idx) => (
+    <ListGroup.Item key={idx}>
+      <Image className="photo" src={x.crestUrl} /> {x.name}
+    </ListGroup.Item>
   ));
+  /*const showTeams = data.teams.map((x, idx) => (
+    <ListGroup>
+      <ListGroup.Item>{x}</ListGroup.Item>
+    </ListGroup>
+  ));*/
 
-  return authenticated ? showTeams : <h1>Login to see teams</h1>;
+  return authenticated ? (
+    <>
+      <Row>
+        <Col sm={3}>
+          <ListGroup> {goThrough}</ListGroup>
+        </Col>
+      </Row>
+    </>
+  ) : (
+    <h1>Login to see teams</h1>
+  );
 }
 
 const mapStateToProps = (state) => ({
