4604abaa302b31bcf3ec7e67d0777c6a960460b1
mbravofuentes <mbravofuentes@mavs.coloradomesa.edu>
2021-04-21 13:21:35 -0600

    deploy

--- soccer101/server.js
+++ soccer101/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const cors = require("cors");
+const path = require("path");
 const app = express();
 
 //Connect Databse
@@ -10,10 +11,6 @@ connectDB();
 app.use(express.json({ extended: false }));
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("API Running");
-});
-
 //Define Routes
 //A way to make the app methods much cleaner, we use app.use and inside the route we use we have the required method for each route
 app.use("/api/users", require("./routes/api/users"));
@@ -21,6 +18,16 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/teams", require("./routes/api/teams"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
