f7ba5ebc8e2ca43c4ff3c259e88995d586d7dbd4
mbravofuentes <mbravofuentes@mavs.coloradomesa.edu>
2021-04-24 22:51:57 -0600

    Update UI and can load posts

--- client/src/components/Forum.js
+++ client/src/components/Forum.js
@@ -1,4 +1,4 @@
-import { CardDeck, Card } from "react-bootstrap";
+import { CardDeck, Card, Button } from "react-bootstrap";
 import { useEffect } from "react";
 import { loadDiscussions } from "../actions/auth";
 import { connect, useDispatch } from "react-redux";
@@ -8,15 +8,32 @@ function Forum({ posts, isAuthenticated }) {
   useEffect(() => {
     dispatch(loadDiscussions());
   }, [dispatch]);
-  const post = posts.map((x) => <Card> {x.text} </Card>);
-  return (
+  const post = posts.map((x) => (
+    <Card>
+      <Card.Header>{x.name}</Card.Header> {x.text}{" "}
+      <Card.Footer>
+        <Button variant="danger" size="sm">
+          {" "}
+          Delete post
+        </Button>
+        <Button variant="info" size="sm">
+          {" "}
+          Like{" "}
+        </Button>
+      </Card.Footer>
+    </Card>
+  ));
+  return isAuthenticated ? (
     <>
       <CardDeck>{post}</CardDeck>
     </>
+  ) : (
+    <h1> Login to see discussions... </h1>
   );
 }
 
 const mapStateToProps = (state) => ({
+  posts: state.posts,
   isAuthenticated: state.auth.isAuthenticated,
 });
 
