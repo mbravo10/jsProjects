7bf25f4cf898eb77dd5eb25b3d4ae75e571cfa56
mbravofuentes <mbravofuentes@mavs.coloradomesa.edu>
2021-05-27 16:52:02 -0600

    Update

--- soccer101/client/src/components/Forum.js
+++ soccer101/client/src/components/Forum.js
@@ -1,3 +1,40 @@
-export default function Forum() {
-  return <h1>Forums</h1>;
+import { CardDeck, Card, Button } from "react-bootstrap";
+import { useEffect } from "react";
+import { loadDiscussions } from "../actions/auth";
+import { connect, useDispatch } from "react-redux";
+
+function Forum({ posts, isAuthenticated }) {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadDiscussions());
+  }, [dispatch]);
+  const post = posts.map((x) => (
+    <Card>
+      <Card.Header>{x.name}</Card.Header> {x.text}{" "}
+      <Card.Footer>
+        <Button variant="info" size="sm" style={{ marginRight: "360px" }}>
+          {" "}
+          Like{" "}
+        </Button>
+        <Button variant="danger" size="sm">
+          {" "}
+          Delete post
+        </Button>
+      </Card.Footer>
+    </Card>
+  ));
+  return isAuthenticated ? (
+    <>
+      <CardDeck>{post}</CardDeck>
+    </>
+  ) : (
+    <h1> Login to see discussions... </h1>
+  );
 }
+
+const mapStateToProps = (state) => ({
+  posts: state.posts,
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps, { loadDiscussions })(Forum);
