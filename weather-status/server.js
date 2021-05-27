935f3c040dda10f077358dc43c1ea68273459fe1
mbravofuentes <mbravofuentes@mavs.coloradomesa.edu>
2021-05-27 17:21:18 -0600

    Adding API keys, and back-end structure

--- weather-status/server.js
+++ weather-status/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
+const weather = require("./api/router");
+
 const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World, this is backend for Weather Info");
 });
+
+app.use("/weather", weather);
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
