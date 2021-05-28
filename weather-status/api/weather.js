155428539181b61d983a7d1fac1d21005ce034a9
mbravofuentes <mbravofuentes@mavs.coloradomesa.edu>
2021-05-27 23:23:57 -0600

    Adding route to weather

---
+++ weather-status/api/weather.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+const api = "";
+
+router.post("/", (req, res) => {
+  var city = req.body.city;
+});
+
+module.exports = router;
--- weather-status/server.js
+++ weather-status/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const weather = require("./api/router");
+const weather = require("./api/weather");
 
 const PORT = 3000;
 
