7691d427c58a6883c0fecdaa7cc7801ec8f11fb6
mbravofuentes <mbravofuentes@mavs.coloradomesa.edu>
2021-05-28 00:24:37 -0600

    Added API KEY to backend to use in front

--- weather-status/api/weather.js
+++ weather-status/api/weather.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
-const api = "";
+const config = require("config");
+const API = "https://api.openweathermap.org/data/2.5/weather";
+const KEY = config.get("WEATHER");
 
-router.post("/", (req, res) => {
-  var city = req.body.city;
+router.get("/", async (req, res) => {
+  try {
+    var city = "Denver";
+    const info = await axios.get(API, {
+      params: { q: city, appid: KEY },
+    });
+
+    res.json(info.data);
+  } catch (e) {
+    console.log(e);
+    res.send("There was an error");
+  }
 });
 
 module.exports = router;
--- weather-status/server.js
+++ weather-status/server.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const app = express();
-const weather = require("./api/weather");
+const cors = require("cors");
 
 const PORT = 3000;
 
+app.use(express.json({ extended: false }));
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello World, this is backend for Weather Info");
 });
 
-app.use("/weather", weather);
+app.use("/weather", require("./api/weather"));
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
