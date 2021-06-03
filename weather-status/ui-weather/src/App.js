3c710de7177e54fa61ececa0cc39757df0412a80
mbravofuentes <mbravofuentes@mavs.coloradomesa.edu>
2021-06-03 12:29:54 -0600

    UI update

--- weather-status/ui-weather/src/App.js
+++ weather-status/ui-weather/src/App.js
@@ -1,4 +1,3 @@
-import { Button } from "react-bootstrap";
 import CardData from "./main/weather";
 
 function App() {
--- weather-status/ui-weather/src/main/weather.js
+++ weather-status/ui-weather/src/main/weather.js
@@ -1,4 +1,4 @@
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, InputGroup, FormControl } from "react-bootstrap";
 import axios from "axios";
 import { useEffect, useState } from "react";
 var startCase = require("lodash.startcase");
@@ -7,6 +7,7 @@ function CardData() {
   const [data, setData] = useState({
     temp: "",
     city: "",
+    cityT: "",
     idCon: "",
     desc: "",
   });
@@ -14,6 +15,10 @@ function CardData() {
   useEffect(() => {
     dataInfo();
   }, []);
+
+  const onChange = (e) => {
+    setData({ ...data, cityT: e.target.value });
+  };
 
   const dataInfo = async () => {
     try {
@@ -24,6 +29,28 @@ function CardData() {
         city: info.data.name,
         idCon: info.data.weather[0].icon,
         desc: startCase(info.data.weather[0].description),
+      });
+    } catch (e) {
+      console.log(e);
+      setData({
+        temp: "",
+        city: "",
+        idCon: "",
+        desc: "Sorry, could not fetch data",
+      });
+    }
+  };
+
+  const cityInfo = async () => {
+    try {
+      const res = await axios.get(
+        "http://localhost:2000/weather/" + data.cityT
+      );
+      setData({
+        temp: res.data.main.temp,
+        city: res.data.name,
+        idCon: res.data.weather[0].icon,
+        desc: startCase(res.data.weather[0].description),
       });
     } catch (e) {
       console.log(e);
@@ -48,6 +75,14 @@ function CardData() {
         <Card.Text>{data.temp} °F</Card.Text>
         <Card.Text>{data.desc} </Card.Text>
       </Card.Body>
+      <InputGroup onChange={onChange} className="mb-3">
+        <InputGroup.Prepend>
+          <Button variant="outline-secondary" onClick={cityInfo}>
+            Get Info
+          </Button>
+        </InputGroup.Prepend>
+        <FormControl aria-describedby="basic-addon1" />
+      </InputGroup>
       <Button onClick={dataInfo}> Fetch for data</Button>
     </Card>
   );
